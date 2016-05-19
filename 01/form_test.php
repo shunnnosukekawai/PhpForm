11eb8ca91ab2cbfdd9848d2fe373020f5743c1cd
shunnosuke kawai <gjpamtp@gmail.com>
2016-05-19 13:55:35 +0900

    20150519

---
+++ 01/form_test.php
@@ -0,0 +1,15 @@
+
+<form action="form_test2.php" method="get">
+
+<p>姓：<input type="textbox" name="name1"　></p>
+<p>名：<input type="textbox" name="name2"　></p>
+<p>電話番号<input type="textbox" name="phone1">
+<input type="textbox" name="phone2"><input type="textbox" name="phone3">
+</p>
+
+    <input type="submit"　>
+
+
+
+
+</form>
---
+++ 01/form_test2.php
@@ -0,0 +1,11 @@
+<?php
+$name1 = $_GET["name1"];
+$name2 = $_GET["name2"];
+echo $name1 . $name2 . "<br>";
+
+echo $_GET["phone1"] . "-" . $_GET["phone2"] . "-" . $_GET["phone3"];
+
+
+
+
+ ?>
